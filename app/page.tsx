df5170f17b118527c52c298d1d4ff875ce2626f1
theahmetgg <ahmet1998gulmez@gmail.com>
2025-06-05 01:58:29 +0300

    home routes updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,32 @@
-import { Link } from "lib/transition"
+// import { Link } from "lib/transition"
 
+// import { PageRoutes } from "@/lib/pageroutes"
+// import { buttonVariants } from "@/components/ui/button"
+
+// export default function Home() {
+//   return (
+//     <section className="flex min-h-[86.5vh] flex-col items-center justify-center px-2 py-8 text-center">
+//       <h1 className="mb-4 text-4xl font-bold sm:text-7xl">Documents</h1>
+//       <p className="text-foreground mb-8 max-w-[600px] sm:text-base">
+//         A simple open-source product documentation platform. That&apos;s simple
+//         to use and easy to customize.
+//       </p>
+
+//       <div className="flex items-center gap-5">
+//         <Link
+//           href={`/docs${PageRoutes[0].href}`}
+//           className={buttonVariants({ className: "px-6", size: "lg" })}
+//         >
+//           Get Started
+//         </Link>
+//       </div>
+//     </section>
+//   )
+// }
+
+import { redirect } from "next/navigation"
 import { PageRoutes } from "@/lib/pageroutes"
-import { buttonVariants } from "@/components/ui/button"
 
 export default function Home() {
-  return (
-    <section className="flex min-h-[86.5vh] flex-col items-center justify-center px-2 py-8 text-center">
-      <h1 className="mb-4 text-4xl font-bold sm:text-7xl">Documents</h1>
-      <p className="text-foreground mb-8 max-w-[600px] sm:text-base">
-        A simple open-source product documentation platform. That&apos;s simple
-        to use and easy to customize.
-      </p>
-
-      <div className="flex items-center gap-5">
-        <Link
-          href={`/docs${PageRoutes[0].href}`}
-          className={buttonVariants({ className: "px-6", size: "lg" })}
-        >
-          Get Started
-        </Link>
-      </div>
-    </section>
-  )
+  redirect(`/docs${PageRoutes[0].href}`)
 }
